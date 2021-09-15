e7c326a4818b7850238e74beb21efaea1751b5eb
vails <vailsoft@hotmail.com>
2021-09-15 12:15:45 -0300

    Concluído

--- src/interfaceimpressora/Impressora.java
+++ src/interfaceimpressora/Impressora.java
@@ -2,17 +2,23 @@ package interfaceimpressora;
 
 public abstract class Impressora {
     public static Imprimivel[] lista = new Imprimivel[3];
-    private static int posLivre = -1;
 
-    public static void imprimirTudo() {
-        for(int i = 0; i<=posLivre; i++){
-            lista[i].imprimir();
-            System.out.println("");
+    public static void adicionarImprimivel(Imprimivel umImprimivel) {
+        for(int i = 0; i < lista.length; i++){
+            if(lista[i] == null){
+                lista[i] = umImprimivel;
+                break;
+            }
         }
     }
-    public static void adicionarImprimivel(Imprimivel umImprimivel) {
-        posLivre++;
-        lista[posLivre] = umImprimivel;
-    }
 
+    public static void imprimirTudo() {
+        for (Imprimivel imprimivel : lista) {
+            if (imprimivel == null) {
+                break;
+            } else {
+                imprimivel.imprimir();
+            }
+        }
+    }
 }
